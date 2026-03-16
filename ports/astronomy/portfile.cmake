71eb407f5cb868dc94955f149daf96dc19b7780d
Cerwym <1760289+Cerwym@users.noreply.github.com>
2026-03-16 14:32:59 +0000

    fix: correct LICENSE filename for astronomy

--- ports/astronomy/portfile.cmake
+++ ports/astronomy/portfile.cmake
@@ -61,4 +61,4 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 # Install license
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
